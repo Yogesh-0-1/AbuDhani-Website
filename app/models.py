2aff522690c11aacfc88a6cb45aafcbaa2b625ad
Yogesh <sainiyogesh2101@gmail.com>
2022-06-30 12:49:52 +0530

    Create app in django project & create static structure of AbuDhani website

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from multiprocessing.spawn import import_main_path
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator , MinValueValidator
+
+# Create your models here.
+
