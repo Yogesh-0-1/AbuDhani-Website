f4edf6a8cbff790f011e53d25c628ded559fa358
Yogesh <sainiyogesh2101@gmail.com>
2022-07-14 12:32:53 +0530

    Work on the login & logout pages with all validatiins and also changes in nav bar if user is log in then show cart or user details otherwise show login or registration button.Then create MyPasswordChange class in forms.py for change password option and also create new html page with name passwordchange.html

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,24 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm , AuthenticationForm,UsernameField
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm,UsernameField ,PasswordChangeForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _ , gettext  
+from django.contrib.auth import password_validation
 
 class CustomerRegistrationForm(UserCreationForm):
-    password1: forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}),label='Password')
-    password2: forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}),label=' Confirm Password (again)')
+    password1: forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2: forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     email = forms.CharField(required=True,widget=forms.EmailInput(attrs={'class':'form-control'}))
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
-        labels={'email':'Email','username':'Username'}
+        labels={'email':'Email','username':'Username','password1':'Password','password2':'Confirm Password'}
         widgets={'username':forms.TextInput(attrs={'class':'form-control'})}
     
 class LoginForm(AuthenticationForm):
     username=UsernameField(widget=forms.TextInput(attrs={'autofocus':True,'class':'form-control'})) 
     password=forms.CharField(label=_("Password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'})) 
+    
+class MyPasswordChange(PasswordChangeForm):
+    old_password=forms.CharField(label=_("Old password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'current-password','autofocus':True,'class':'form-control'}))
+    new_password1=forms.CharField(label=_("New password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}),help_text=password_validation.password_validators_help_text_html())
+    new_password2=forms.CharField(label=_("New password confirmation"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}))    
--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app import views
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .forms import LoginForm
+from .forms import LoginForm, MyPasswordChange
 urlpatterns = [
     # path('', views.home),
     path('', views.ProductView.as_view(), name='home'),
@@ -16,6 +16,8 @@ urlpatterns = [
     path('changepassword/', views.change_password, name='changepassword'),
     path('sherwani/<slug:data>', views.sherwanis, name='sherwanidata'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',form_class=MyPasswordChange), name='passwordchange'),
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
