e5895a8ebc39bd9b40e8d55e4534f7d9f2d40da4
yogeshsaini <sainiyogesh2101@gmail.com>
2023-07-03 22:44:05 +0530

    initial configration of Razorpay payment

--- ABU_DHANI_WEBSITE/settings.py
+++ ABU_DHANI_WEBSITE/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=hufwn4^9b(%_(&8x%=l(mjpct$p&7xbrfkl4dx1c6(r9nkf6'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG' , cast=bool)
 
 ALLOWED_HOSTS = []
 
